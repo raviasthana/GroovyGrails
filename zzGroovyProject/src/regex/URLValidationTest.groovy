6caaf95af6f07f1a8bfb5c7802f8b4a3f9ce7258
raviasthana <ravi.asthana@gmail.com>
2016-11-07 17:13:19 +0000

    url regex test

---
+++ zzGroovyProject/src/regex/URLValidationTest.groovy
@@ -0,0 +1,33 @@
+package regex
+
+class URLValidationTest {
+	
+	static void main(def args){
+		/*
+		String url = "example.com"
+		String url = "sub.example.com"
+		String url = "sub.domain.my-example.com"
+		String url = "sub.domain.my-example.paris"
+		String url = "example.com/?stuff=true"
+		String url = "example.com:5000/?stuff=true"
+		String url = "sub.domain.my-example.com/path/to/file/hello.html"
+		String url = "hello.museum"
+		String url = "http://railsgirls.com"
+		String url = "abc@cde"
+		*/
+		String url = "sub.domain.my-example.paris"
+		if (!url.startsWith('http://')) url='http://'+url
+		
+		if(isURLValid(url)){
+			println "VALID url"
+		}else{
+			println "INVALID url"
+		}
+	}
+	
+	static Boolean isURLValid(url){
+		//url ==~/\b(https?|ftp|file):\/\/[-A-Za-z0-9+&@#\/%?=~_|!:,.;]*[-A-Za-z0-9+&@#\/%=~_|]/ //without .com/.org/.uk etc
+		url ==~ /\b(https?|ftp|file):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.([a-z]+)?(\/.*)?$/ //without port number
+		//url ==~ /\b(https?|ftp|file):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]+(:[0-9]{1,5})?(\/.*)?$/ //with port number
+	}
+}
